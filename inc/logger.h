7753c90b8060be81d8552d1c0553e2692f79082c
RenatoBrittoAraujo <renatobritto@protonmail.com>
2022-03-07 20:14:39 -0300

    Add headers

--- inc/logger.h
+++ inc/logger.h
@@ -1,11 +1,24 @@
-#ifndef LOG_H_
-#define LOG_H_
+#ifndef LOGGER
+#define LOGGER
 
 #include <string>
 
-void log_temperature(float TI, float TR, float TE);
-void log_message(std::string mensagem);
-void kill_logger();
-void init_logger();
+class Logger
+{
+public:
+    Logger();
+    ~Logger();
+    
+    void log_temperature(float TI, float TR, float TE);
+    void log_message(std::string mensagem);
+    void kill_logger();
+
+    static Logger get_instance();
+    static Logger _instance;
+
+private:
+    std::ofstream file;
+    std::string get_timestamp();
+}
 
 #endif
